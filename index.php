cc63bce70c92c7def294563d8aade62489787dad
mwicaksono <mwicaksono99@gmail.com>
2021-07-09 14:00:06 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -11,6 +11,10 @@
 <body>
     <h1> Helo wourld</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum qui veniam ad vero. Necessitatibus tempora qui dolorum? Magnam necessitatibus aliquid eaque iste facilis dolore veritatis sed quo, vel commodi laudantium ad consequatur id, doloremque fuga nemo fugiat iure beatae placeat recusandae. Neque culpa a cupiditate ad, modi placeat aut. Illum facilis id quia nam nisi nesciunt distinctio magnam praesentium sunt, quibusdam dolorum consectetur, repudiandae minima quos aperiam voluptatem fuga. Quaerat numquam officia rem cum voluptatem molestiae eligendi in magni veritatis a fuga blanditiis vero odio ea quis laborum, alias dolor doloremque sapiente voluptatum quisquam! Odio, dolores? Soluta laboriosam aliquam nulla ipsa repudiandae excepturi perspiciatis, eligendi sit fuga ut? Voluptatibus perferendis odit, praesentium voluptates dolorem omnis ullam pariatur assumenda impedit totam eligendi molestiae deleniti, adipisci nobis voluptatum natus asperiores illo odio laudantium optio sed facere est laboriosam fuga. Dolore officia velit reprehenderit, exercitationem esse sit distinctio molestias nostrum vero, atque et repudiandae reiciendis error inventore. Omnis autem nisi dicta facilis minus maxime quidem doloribus, voluptatum quasi quibusdam saepe dolore, aliquam, unde facere! Facilis odio maiores accusamus cumque expedita nostrum, aliquid assumenda eveniet, quibusdam id commodi animi eum. Odit, tenetur. Reiciendis commodi quidem explicabo voluptas quod praesentium dolores natus suscipit soluta perferendis?</p>
+
+    <ul>
+        <li>Enak</li>
+    </ul>
 </body>
 
 </html>
